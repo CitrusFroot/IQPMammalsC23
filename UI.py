818eb720f81e8e3c4cfafb7ce7577db0bf378456
Jacob Reiss <jtreiss@wpi.edu>
2023-01-19 15:55:36 +0200

    added basic UI for the system. It reports the name of a folder.

---
+++ UI.py
@@ -0,0 +1,24 @@
+import PySimpleGUI as sg
+from tkinter import filedialog as fd
+
+layout = [[sg.Text("Please choose the folder with photos to analyze:")], 
+          [sg.Button("Browse Files")], 
+          [sg.Button("CANCEL")]]
+
+# Create the window
+window = sg.Window("DAWG 0.1", layout)
+
+# Create an event loop
+while True:
+    event, values = window.read()
+    # End program if user closes window or
+    # presses the OK button
+    if  event == sg.WIN_CLOSED or event == "CANCEL":
+        break
+    elif event == "Browse Files":
+        filename = fd.askdirectory()
+        #TODO: call CNN to run this application
+        print(filename)
+
+
+window.close()
