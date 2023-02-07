4068a134d0a7f6ce63fd6706f5319bbe3e84989c
Jacob Reiss <jtreiss@wpi.edu>
2023-02-07 15:07:41 +0200

    managed to get images to load properly, images DO NOT get processed yet !!!NOT STABLE!!!

--- cnnvgg16.py
+++ cnnvgg16.py
@@ -132,6 +132,7 @@ model.summary() #prints out a summary table
 #runs the model and saves it as a History object
 es1 = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience = 3) #stops training the network if overfitting occurs
 hist = model.fit(x = trainData[0],         #these numbers need to be experimented with 
+<<<<<<< HEAD
 
                  steps_per_epoch = 30, 
                  epochs = 15,
@@ -139,6 +140,11 @@ hist = model.fit(x = trainData[0],         #these numbers need to be experimente
                  validation_data = trainData[1],
                  validation_steps = 10, 
 
+=======
+                 steps_per_epoch = 10, 
+                 epochs = 5, 
+                 validation_steps = 10, 
+>>>>>>> 9266971 (managed to get images to load properly, images DO NOT get processed yet !!!NOT STABLE!!!)
                  verbose = 1)           #should be 2 in final system
 
 model.save('vgg16Run.h5') #saves the model as a readable file
