e78ab4869dc2dcff1f5985496a48c4c77f68ef62
Jacob Reiss <jtreiss@wpi.edu>
2023-02-07 13:27:35 +0200

    updated files to load in a saved model, prompt the user for a directory, and analyze photos individually. !!NOT STABLE!!

--- cnnvgg16Implementation.py
+++ cnnvgg16Implementation.py
@@ -1,15 +1,32 @@
 from numpy import loadtxt
 import tensorflow
+from keras.utils import img_to_array
 from keras.models import load_model
+import os
+
+
+def makeCSV():
+    file = open('labeledCandids.csv', 'w')
+    file.write('File,RelativePath,DateTime,DeleteFlag,CameraNumber,DayNight,Animal,Count\n')
+    file.close()
+    print('success!')
+
+def decoder(arr):
+
+    print('success!')
 
 CUTOFF = 0.7
 
 def runModel(mainDIR):
     cnn = load_model('vgg16Run.h5')
-    prediction = cnn.predict(mainDIR)
-    print(prediction)
 
-    for i in prediction:
-        if(i > CUTOFF):
-            species = prediction.get
+    for images in os.scandir(mainDIR):
+        if  not images.is_file():
+            print()
+        
+        else:
+            fileType = images.name.split('.')[1]
+            if fileType == 'png' or fileType == 'jpg' or fileType == 'bmp' or fileType == 'gif':
+                arr = cnn.predict(img_to_array(images))
+                print(arr)
         
