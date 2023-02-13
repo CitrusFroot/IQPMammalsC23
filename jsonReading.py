a92a707e0baa99bbac614ee23bdecc21706d3cdd
Jacob Reiss <jtreiss@wpi.edu>
2023-02-13 11:09:43 +0200

    successfully extracted bounding boxes of the json file. Only does so for the first bounding box

--- jsonReading.py
+++ jsonReading.py
@@ -1,12 +1,14 @@
 import json
-def extractBoundingBoxes():
+def extractBoundingBoxes(): #TODO: get count of detections, record confidence of detections, etc.
     boundingBoxes = []
 
     jsonFile = open('image_recognition_file.json')
     data = json.load(jsonFile)
     for image in data["images"]:
-        for detections in image["detections"]:
-            bboxes = detections[]
+        if(len(image["detections"] > 0))
+        detection1 = image["detections"][0] #first detection.
+        bbox = detection1["bbox"] #bounding boxes in form []
+        boundingBoxes.append(bbox)
 
 
     return boundingBoxes
