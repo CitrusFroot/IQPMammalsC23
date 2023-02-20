05809c43422cd3187801d5f670c557c8746ffaf9
Jacob Reiss <jtreiss@wpi.edu>
2023-02-20 16:49:04 +0200

    fixed code to only extract the count of animals

--- jsonReading.py
+++ jsonReading.py
@@ -1,24 +1,19 @@
 import json #needed to read json files
 
-#reads a json file in the same directory as code and extracts bounding boxes listed in the file
-#returns: a list of lists of bounding boxes
-def extractBoundingBoxes(): #TODO: get count of detections, record confidence of detections, etc.
-    CONFIDENCETHRESHOLD = 0.5
-    boundingBoxes = [] #empty starting list; gets returned
+#gets the count of animals in an image
+#mainDIR: a string that represents the directory with the images
+#returns: a list of integers representing the count of animal detections in each image
+def getCount(mainDIR):
+    counts = [] #list that gets returned
+    jsonFile = open(mainDIR + '/image_recognition_file.json') #opens the JSON file
+    data = json.load(jsonFile) #accesses the data in the json file
 
-    jsonFile = open('image_recognition_file.json') #opens the json file
-    data = json.load(jsonFile) #converts the json file into a python readable format (objects -> dicts, etc. View documentation for more info)
-    
-    #for each image in data (json file)
-    #for each detection in each image
-    #if the program was confident in its detection (>=0.7), and it is the best confidence yet,
-    #set bbox to be that detection's bounding box
+    #for every image dictionary in the images dictionary in data
+    #increase count if a detection dictionary in the image dictionary has a confidence >= 0.1, and is category 1 (animal)
     for image in data["images"]:
-        bbox = [] #empty list; default value
-        bestConf = CONFIDENCETHRESHOLD #used for boolean logic
+        count = 0 #gets added to counts
         for detection in image["detections"]:
-            if (detection["conf"] >= bestConf): #if we are confident in the detection, do:
-                bbox = detection["bbox"]
-                bestConf = detection["conf"]
-        boundingBoxes.append(bbox) #add bbox to boundingBoxes, even if bbox = []
-    return boundingBoxes
+            if(detection["conf"] >= .1 and detection["category"] == 1): #if we are confident in the detection, and the detection is category 1 (animal), do:
+                count += 1 #increase the count
+        counts.append(count) #add the final count to counts
+    return counts
