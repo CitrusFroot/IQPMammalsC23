d4e3c7388b5505aed4ea7464d853a21cb3672707
Jacob Reiss <jtreiss@wpi.edu>
2023-02-22 20:28:17 +0200

    fixed error message when closing the application, and removed unnecessary debug messages

--- driver.py
+++ driver.py
@@ -5,18 +5,18 @@ import jsonReading as jr
 CUTOFF = 0.3
 
 choiceTuple = ui.loadUI() #gets a tuple of both the directory of interest, and a value (None, 0, 1) determining what to do
+if(choiceTuple[1] != ""):
+    match choiceTuple[0]:
+        case 0:
+            predictions = cnn.runModel(choiceTuple[1])
+            count = jr.getCount(choiceTuple[1])
+            print(count)
+            cnn.makeCSV(choiceTuple[1], predictions, count, CUTOFF)
+        case 1:
+            print("currently not enabled. Sorry!")
+            for i in range(700):
+                beans = True
 
-match choiceTuple[0]:
-    case 0:
-        predictions = cnn.runModel(choiceTuple[1])
-        count = jr.getCount(choiceTuple[1])
-        print(count)
-        cnn.makeCSV(choiceTuple[1], predictions, count, CUTOFF)
-    case 1:
-        print("currently not enabled. Sorry!")
-        for i in range(700):
-            beans = True
-
-        #cnn.retrain(choiceTuple[1])
-    case _:
-        print('no choice made- everything is all good. closing application!')
+            #cnn.retrain(choiceTuple[1])
+        case _:
+            print('no choice made- everything is all good. closing application!')
